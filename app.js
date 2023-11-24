efb2cf06ccc06aaceb41cc3a2205ea01b12d1321
jrushi2010 <jrushi2010@gmail.com>
2023-11-24 13:59:10 +0530

    refactoring routes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const students = JSON.parse(
 
 app.use(express.json());
 
-app.get('/api/v1/students', (req, res) => {
+const getAllStudents = (req, res) => {
     res.status(200).json({
         status: 'success',
         results: students.length,
@@ -18,9 +18,32 @@ app.get('/api/v1/students', (req, res) => {
             students
         }
     });
-});
+}
 
-app.post('/api/v1/students', (req, res) => {
+const getStudent = (req, res) => {
+    console.log(req.params);
+
+    const id = req.params.id * 1;
+
+    const student = students.find(el => el.id === id);
+
+    // if (id > students.length) {
+    if (!student) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            student
+        }
+    });
+}
+
+const createStudent = (req, res) => {
     //console.log(req.body);
     const newId = students[students.length - 1].id + 1;
 
@@ -37,31 +60,60 @@ app.post('/api/v1/students', (req, res) => {
                 }
             })
     })
-});
+}
 
-app.get('/api/v1/students/:id', (req, res) => {
-    console.log(req.params);
+const updateStudent = (req, res) => {
 
     const id = req.params.id * 1;
 
-    const student = students.find(el => el.id === id);
-
-    // if (id > students.length) {
-    if (!student) {
+    if (id > students.length) {
         return res.status(404).json({
             status: 'failed',
             message: 'Invalid ID'
         });
     }
 
+    res.status(200)
+        .json({
+            status: 'success',
+            data: {
+                student: '<updated students is here...>'
+            }
+        })
+}
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            student
-        }
-    });
-});
+const deleteStudent = (req, res) => {
+
+    const id = req.params.id * 1;
+
+    if (id > students.length) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(204)
+        .json({
+            status: 'success',
+            data: null
+        });
+}
+
+// app.get('/api/v1/students', getAllStudents);
+// app.get('/api/v1/students/:id', getStudent);
+// app.post('/api/v1/students', createStudent);
+// app.patch('/api/v1/students/:id', updateStudent);
+// app.delete('/api/v1/students/:id', deleteStudent);
+
+app.route('/api/v1/students')
+    .get(getAllStudents)
+    .post(createStudent);
+
+app.route('/api/v1/students/:id')
+    .get(getStudent)
+    .patch(updateStudent)
+    .delete(deleteStudent);
 
 app.listen(port, () => {
     console.log(`app running on port ${port}...`);
