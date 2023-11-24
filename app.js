56a6705f262cb65d4b5ce8ad63bbf29974671cee
jrushi2010 <jrushi2010@gmail.com>
2023-11-24 13:24:56 +0530

    added post method

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const students = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data.json`)
 );
 
+app.use(express.json());
+
 app.get('/api/v1/students', (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -17,6 +19,25 @@ app.get('/api/v1/students', (req, res) => {
         }
     });
 });
+
+app.post('/api/v1/students', (req, res) => {
+    //console.log(req.body);
+    const newId = students[students.length - 1].id + 1;
+
+    const newStudent = Object.assign({ id: newId }, req.body);
+
+    students.push(newStudent);
+
+    fs.writeFile(`${__dirname}/dev-data/data.json`, JSON.stringify(students), err => {
+        res.status(201)
+            .json({
+                status: 'success',
+                data: {
+                    student: newStudent
+                }
+            })
+    })
+})
 
 app.listen(port, () => {
     console.log(`app running on port ${port}...`);
