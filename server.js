60a66f057cc5de0f6abf5831572fc9bc37fd5951
jrushi2010 <jrushi2010@gmail.com>
2023-11-25 10:01:56 +0530

    database connection done successfullt

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const dotenv = require('dotenv');
 
@@ -5,9 +6,46 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+}).then(con => {
+    //console.log(con.connections);
+    console.log('DB connection successfull');
+});
 
-console.log(process.env);
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A student must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    fees: {
+        type: Number,
+        required: [true, 'A student must have a fees']
+    }
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+const testStudent = new Student({
+    name: 'Rohit patil',
+    rating: 4.7,
+    fees: 497
+});
+
+testStudent.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log('error : ', err)
+})
+
+//console.log(process.env);
 
 const port = process.env.PORT || 5000;
 
